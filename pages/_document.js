67118631190316a12a92cdef8493b4502d76004d
Anurag Kumar Singh <singh.ask@outlook.com>
2023-01-08 20:19:58 +0530

    code

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,7 @@ export default function Document() {
         <meta property="og:title" content="AI Avatar Generator" key="title" />
         <meta
           property="og:description"
-          content="build with buildspace"
+          content="Anurag's Ai image Gen"
           key="description"
         />
         <meta name="twitter:card" content="summary_large_image"></meta>
--- pages/index.js
+++ pages/index.js
@@ -3,33 +3,52 @@ import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
 
 const Home = () => {
+  const [input, setInput] = useState('');
+  const onChange = (event) => {
+    setInput(event.target.value);
+  };
+  // Add generateAction
+  const generateAction = async () => {
+    console.log('Generating...');	
+  }
+  
   return (
     <div className="root">
       <Head>
-        <title>AI Avatar Generator | buildspace</title>
+        <title>AI Avatar Generator | Anurag</title>
       </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>your generator one-liner</h1>
+            <h1>The Cool image Generator!📈🗿</h1>
           </div>
           <div className="header-subtitle">
-            <h2>description of your generator</h2>
+            <h2> Turn me into anyone you want! Make sure you refer to me as "Anurag" in the prompt</h2>
           </div>
+          <div className="prompt-container">
+        <input className="prompt-box" value={input} onChange={onChange} />
+      </div>
         </div>
+        <div className="prompt-buttons">
+    <a className="generate-button" onClick={generateAction}>
+      <div className="generate">
+        <p>Generate</p>
+      </div>
+    </a>
       </div>
       <div className="badge-container grow">
         <a
-          href="https://buildspace.so/builds/ai-avatar"
+          href="https://huggingface.co/anurag-bit/Ai-avatar-Generator"
           target="_blank"
           rel="noreferrer"
         >
           <div className="badge">
             <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>build with buildspace</p>
+            <p>built by Anurag📈🗿</p>
           </div>
         </a>
       </div>
+    </div>
     </div>
   );
 };
